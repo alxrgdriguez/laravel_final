7bcd5cbd128c8f56f9b52a5f7eb484ae0b0c2687
alxrgdriguez <sralexred@gmail.com>
2025-03-12 00:17:56 +0100

    Terminado :D

--- Formacion/routes/web.php
+++ Formacion/routes/web.php
@@ -68,16 +68,16 @@ Route::post('/profile/generate-token', [ProfileController::class, 'generateToken
     ->middleware('auth')
     ->name('profile.generate-token');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-
 Route::middleware('auth')->group(function () {
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+/*Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');*/
+
 
 require __DIR__.'/auth.php';
